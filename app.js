f340a51bd0f72a15743ed5e01f3d7f4a8b7fb4eb
Stephen Yu <stephenyu1229@gmail.com>
2023-03-31 16:52:13 -0400

    hook up yelp api for searching

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
-import RxController from "./controllers/restaurant-controller";
+import cors from "cors";
+import RxController from "./controllers/restaurant-controller.js";
 
 const app = express();
-const port = 4000;
+app.use(cors());
+app.use(express.json())
+const PORT = process.env.PORT || 4000;
 
 RxController(app);
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost: ${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost: ${PORT}`);
 });
--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,15 +1,32 @@
-const YELP_REST_API_URL = "https://api.yelp.com/v3";
+import axios from "axios";
+
+const YELP_RXS_REST_API_URL = "https://api.yelp.com/v3";
+const HEADER = {
+  headers: {
+    Authorization: `Bearer Wm0L8O-5Vq0xe6Q0qW37yUeC8DzlJVsD7NSXy4g8alZDLXKf7o_20n7hijKvgG0twYcWl_0oVlTC8JXt9cedwQzriGyhT74ZeCEl8vVWBUM8AGrenC9NOFZeEJUkZHYx`,
+  },
+};
 
 const RxController = (app) => {
-    const findRxs = (req, res) => {
-        const { term, location, priceRange } = req.body;
+  const findRxs = async (req, res) => {
+    const { term, location, priceRange } = req.query;
 
-    };
+    let apiQuery = `${YELP_RXS_REST_API_URL}/businesses/search?`;
+    if (term) {
+      apiQuery += `term=${encodeURIComponent(term)}&`;
+    }
+    if (location) {
+      apiQuery += `location=${encodeURIComponent(location)}&`;
+    }
+    if (priceRange) {
+      apiQuery += `price=${encodeURIComponent(priceRange)}`;
+    }
 
+    const response = await axios.get(apiQuery, HEADER);
+    res.json(response.data.businesses);
+  };
 
-
-
-    app.get("/api/restaurants", findBusinesses);
-}
+  app.get("/api/restaurants", findRxs);
+};
 
 export default RxController;
