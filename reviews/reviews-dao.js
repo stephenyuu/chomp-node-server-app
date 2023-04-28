20d2fd163332808d0c504b08f10fd13da1754dfe
Stephen Yu <stephenyu1229@gmail.com>
2023-04-28 14:56:00 -0400

    fix review dao

--- reviews/reviews-dao.js
+++ reviews/reviews-dao.js
@@ -1,27 +1,26 @@
 import reviewsModel from "./reviews-model.js";
 
-
 export const createReview = async (review) => {
   const newReview = await reviewsModel.create(review);
   return newReview;
 };
 
 export const findReviewsByRxId = async (rxId) => {
-  const reviews = await reviewsModel.find({rxId});
-  return reviews;
-}
-
-export const findReviewsByUserId = async (userId) => {
-  const reviews = await reviewsModel.find({userMongooseKey: userId});
+  const reviews = await reviewsModel.find({ rxId });
   return reviews;
 };
 
-export const deleteReview = async ({ rxId, userId}) => {
-    const status = await reviewsModel.deleteOne({ rxId, userId});
-    return status;
-  };
+export const findReviewsByUserId = async (userId) => {
+  const reviews = await reviewsModel.find({ userMongooseKey: userId });
+  return reviews;
+};
 
-  export const updateReview = async (id, user) => {
-    const status = await reviewsModel.updateOne({ _id: id }, user);
-    return status;
-  };
+export const deleteReview = async (reviewId) => {
+  const status = await reviewsModel.deleteOne({ _id: reviewId });
+  return status;
+};
+
+export const updateReview = async (id, user) => {
+  const status = await reviewsModel.updateOne({ _id: id }, user);
+  return status;
+};
