5fa2ff92f523840b0561cd48a740bb328e28246b
madeline <chew.ma@northeastern.edu>
2023-04-28 09:33:57 -0700

    complete review controller

--- controllers/reviews-controller.js
+++ controllers/reviews-controller.js
@@ -10,19 +10,19 @@ const ReviewsController = (app) => {
       return;
     }
 
-    const { review } = req.body;
+    const reviewText = req.body.text;
 
     let rx = await rxDao.findRxByRxId(req.params.rxid);
 
     if (!rx) {
-      rx = await rxDao.createRx(req.body);
+      rx = await rxDao.createRx({name: req.params.rxname, rxId: req.params.rxid});
     }
 
-    const newReview = await reviewsDao.createReview({
+    const newReview = await reviewDao.createReview({
       userMongooseKey: currentUser._id,
       rxMongooseKey: rx._id,
       rxId: rx.rxId,
-      review,
+      review: reviewText,
     });
 
     res.json(newReview);
@@ -33,15 +33,27 @@ const ReviewsController = (app) => {
     res.json(reviews);
   }
 
-  // finds the REVIEWS by user ID
   const findReviewedRxsOfUser = async (req, res) => {
     const reviews = await reviewDao.findReviewsByUserId(req.params.userid);
     res.json(reviews);
   };
 
-  app.put("/api/reviews/:rxid/", reviewRx);
+  const updateReview = async (req, res) => {
+    const review = req.body.text;
+    const status = await reviewDao.updateReview(req.params.reviewid, review);
+    res.send(status);
+  };
+
+  const deleteReview = async (req, res) => {
+    const status = await reviewDao.deleteReview(req.params.reviewid);
+    res.send(status);
+  };
+
+  app.post("/api/reviews/:rxid/:rxname", reviewRx);
+  app.put("/api/reviews/:reviewid", updateReview)
   app.get("/api/reviews/rxs/:rxid", findReviewsOfRx);
   app.get("/api/reviews/user/:userid", findReviewedRxsOfUser);
+  app.delete("/api/reviews/delete/:reviewid", deleteReview);
 
 };
 
